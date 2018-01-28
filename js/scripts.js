6fa7cb6487034db7669b0c5cbd7a6b8c993b637e
Adrian Slaby <adrianslaby@gmail.com>
2018-01-28 20:02:02 +0100

    add files

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+// scripts
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+console.log('Hello, ' + name);
+/* var name = prompt('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);*/
